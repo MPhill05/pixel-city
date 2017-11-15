0eea23cdcb646afc5e6278ae01ef9e56490cb3a1
Mark Phillips <mphill05@gmail.com>
2017-11-14 16:21:57 -0800

    Updated plist for location checks, added a file that the login page and create account by email page both route to the check page for accepting location services.

---
+++ pixel-city/Controller/MapVC.swift
@@ -0,0 +1,58 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by Mark Phillips on 11/14/17.
+//  Copyright © 2017 Team Nerd. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
+    }
+
+    @IBAction func centerBtnPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
+    }
+    
+}
+
+extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+}
+
